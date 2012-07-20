17655b65ae024572916647bd98c54e15b05b6f6a
Adam Bowen <adambowen@group360.com>
2012-07-20 16:13:57 -0500

    Add project files

---
+++ src/Sanitize/Filtered.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Filtered provides magic methods to get/set arbitrary data in an object.
+ * Returns a null value for any access to a key that doesn't exist.
+ * 
+ * @author Russell Stringer <r.stringer@gmail.com>
+ * @author Adam Bowen <adamnbowen@gmail.com>
+ * @license http://dbad-license.org/license DBAD license
+ */
+
+namespace Sanitize;
+
+class Filtered
+{
+
+  /**
+   * _filtered the object accessed via __get() and __set()
+   * 
+   * @var array
+   */
+  private $_filtered = array();
+
+  /**
+   * __get If a nonexistent property of a Filtered object is called, this
+   * function checks to see if the property corresponds to a key of
+   * $this->_filtered, and returns that, otherwise it returns null.
+   * 
+   * @param mixed $key 
+   * @return void
+   */
+  public function __get($key)
+  {
+    if (array_key_exists($key, $this->_filtered)
+        && !empty($this->_filtered[$key])
+    ) {
+      return $this->_filtered[$key];
+    } else {
+      return null;
+    }
+  }
+
+  public function __set($key, $value)
+  {
+    $this->_filtered[$key] = $value;
+  }
+
+}
---
+++ src/Sanitize/Sanitize.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Sanitize A class for sanitizing arrays and objects, with the additional
+ * feature of returning null for nonexistent properties.
+ * 
+ * @author Adam Bowen <adamnbowen@gmail.com>
+ * @author Russell Stringer <r.stringer@gmail.com>
+ * @license http://dbad-license.org/license DBAD license
+ */
+
+namespace Sanitize;
+
+use Sanitize/Filtered;
+
+class Sanitize
+{
+
+  /**
+   * Clean Sanitize the keys and values of the $unclean object/array
+   * 
+   * @param mixed $unclean
+   * @return Filtered object containing the sanitized values
+   */
+  public static function Clean($unclean)
+  {
+    $filtered = new Filtered();
+    foreach ($unclean as $key => $value) {
+      $sanitizedKey = self::_sanitize($key);
+      $sanitizedValue = self::_sanitize($value);
+      $filtered->$sanitizedKey = $sanitizedValue;
+    }
+
+    return $filtered;
+  }
+
+  /**
+   * _sanitize Sanitize the given $input.
+   *
+   * Sends all inputs to _fixIncompleteObject() to ensure there are no broken
+   * objects, then if $input is an object or an array, it cleans $input
+   * recursively.  If $input is a string, it is simply cleaned and returned.
+   * 
+   * @param mixed $input 
+   * @return mixed Either a simple cleaned string or a cleaned array.
+   */
+  private function _sanitize($input)
+  {
+    $input = self::_fixIncompleteObject($input);
+
+    if (is_array($input) || is_object($input)) {
+      $output = array();
+      foreach ($input as $key => $value){
+        $output[$key] = self::_sanitize($value);
+      }
+      return $output;
+    }
+    return stripslashes(htmlentities(strip_tags(trim($input))));
+  }
+
+  /**
+   * _fixIncompleteObject repairs an object if it is incomplete.
+   *
+   * Removes the __PHP_Incomplete_Class crap from the object, so is_object()
+   * will correctly identify $input as an object
+   *
+   * @param object $object The "broken" object
+   * @return object The "fixed" object
+   */
+  private function _fixIncompleteObject($input)
+  {
+    if (!is_object($input) && gettype($input) == 'object') {
+      return unserialize(serialize($input));
+    }
+    return $input;
+  }
+
+}
