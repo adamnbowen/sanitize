caa39eaeb2891557c242c24fd46c517c7fbe6b88
Adam Bowen <adambowen@group360.com>
2012-07-26 09:39:11 -0500

    Working on phpcs fixes, both indentation and commenting

--- src/Sanitize/Filtered.php
+++ src/Sanitize/Filtered.php
@@ -3,8 +3,10 @@
  * Filtered provides magic methods to get/set arbitrary data in an object.
  * Returns a null value for any access to a key that doesn't exist.
  *
- * @author Russell Stringer <r.stringer@gmail.com>
- * @author Adam Bowen <adamnbowen@gmail.com>
+ * PHP version 5.3
+ *
+ * @author  Russell Stringer <r.stringer@gmail.com>
+ * @author  Adam Bowen <adamnbowen@gmail.com>
  * @license http://dbad-license.org/license DBAD license
  */
 
@@ -12,36 +14,43 @@ namespace Sanitize;
 
 class Filtered
 {
+    /**
+     * _filtered the object accessed via __get() and __set()
+     *
+     * @var array
+     */
+    private $_filtered = array();
 
-  /**
-   * _filtered the object accessed via __get() and __set()
-   *
-   * @var array
-   */
-  private $_filtered = array();
-
-  /**
-   * __get If a nonexistent property of a Filtered object is called, this
-   * function checks to see if the property corresponds to a key of
-   * $this->_filtered, and returns that, otherwise it returns null.
-   *
-   * @param mixed $key
-   * @return void
-   */
-  public function __get($key)
-  {
-    if (array_key_exists($key, $this->_filtered)
-        && !empty($this->_filtered[$key])
-    ) {
-      return $this->_filtered[$key];
-    } else {
-      return null;
+    /**
+     * __get If a nonexistent property of a Filtered object is called, this
+     * function checks to see if the property corresponds to a key of
+     * $this->_filtered, and returns that, otherwise it returns null.
+     *
+     * @param string $key a possible key to the $_filtered array
+     *
+     * @return mixed null or the value of the existing key
+     */
+    public function __get($key)
+    {
+        if (array_key_exists($key, $this->_filtered)
+            && !empty($this->_filtered[$key])
+        ) {
+            return $this->_filtered[$key];
+        } else {
+            return null;
+        }
     }
-  }
 
-  public function __set($key, $value)
-  {
-    $this->_filtered[$key] = $value;
-  }
-
+    /**
+     * __set If a property of a Filtered object is set, this function sets the 
+     * appropriate key in the $_filtered array, allowing it to be retrieved 
+     * later.
+     *
+     * @param string $key   a new (or existing) key to the $_filtered array
+     * @param mixed  $value the value to set the key of $_filtered to
+     */
+    public function __set($key, $value)
+    {
+        $this->_filtered[$key] = $value;
+    }
 }
--- src/Sanitize/Sanitize.php
+++ src/Sanitize/Sanitize.php
@@ -3,8 +3,10 @@
  * Sanitize A class for sanitizing arrays and objects, with the additional
  * feature of returning null for nonexistent properties.
  *
- * @author Adam Bowen <adamnbowen@gmail.com>
- * @author Russell Stringer <r.stringer@gmail.com>
+ * PHP version 5.3
+ *
+ * @author  Adam Bowen <adamnbowen@gmail.com>
+ * @author  Russell Stringer <r.stringer@gmail.com>
  * @license http://dbad-license.org/license DBAD license
  */
 
@@ -14,71 +16,71 @@ use Sanitize\Filtered;
 
 class Sanitize
 {
+    /**
+     * Clean Sanitize the keys and values of the $unclean object/array
+     *
+     * @param mixed $unclean
+     *
+     * @return Filtered object containing the sanitized values
+     */
+    public static function Clean($unclean)
+    {
+        $filtered = new Filtered();
+        foreach ($unclean as $key => $value) {
+            $sanitizedKey = self::_sanitize($key);
+            $sanitizedValue = self::_sanitize($value);
+            $filtered->$sanitizedKey = $sanitizedValue;
+        }
 
-  /**
-   * Clean Sanitize the keys and values of the $unclean object/array
-   *
-   * @param mixed $unclean
-   * @return Filtered object containing the sanitized values
-   */
-  public static function Clean($unclean)
-  {
-    $filtered = new Filtered();
-    foreach ($unclean as $key => $value) {
-      $sanitizedKey = self::_sanitize($key);
-      $sanitizedValue = self::_sanitize($value);
-      $filtered->$sanitizedKey = $sanitizedValue;
+        return $filtered;
     }
 
-    return $filtered;
-  }
+    /**
+     * _sanitize Sanitize the given $input.
+     *
+     * Sends all inputs to _fixIncompleteObject() to ensure there are no broken
+     * objects, then if $input is an object or an array, it cleans $input
+     * recursively.  If $input is a boolean, it is simply returned.  If $input is
+     * a string, it is simply cleaned and returned.
+     *
+     * @param mixed $input
+     *
+     * @return mixed Either a simple cleaned string or a cleaned array.
+     */
+    private static function _sanitize($input)
+    {
+        $input = self::_fixIncompleteObject($input);
 
-  /**
-   * _sanitize Sanitize the given $input.
-   *
-   * Sends all inputs to _fixIncompleteObject() to ensure there are no broken
-   * objects, then if $input is an object or an array, it cleans $input
-   * recursively.  If $input is a boolean, it is simply returned.  If $input is
-   * a string, it is simply cleaned and returned.
-   *
-   * @param mixed $input
-   * @return mixed Either a simple cleaned string or a cleaned array.
-   */
-  private static function _sanitize($input)
-  {
-    $input = self::_fixIncompleteObject($input);
+        if (is_array($input) || is_object($input)) {
+            $output = array();
+            foreach ($input as $key => $value) {
+                $output[$key] = self::_sanitize($value);
+            }
 
-    if (is_array($input) || is_object($input)) {
-      $output = array();
-      foreach ($input as $key => $value) {
-        $output[$key] = self::_sanitize($value);
-      }
+            return $output;
+        }
+        if (is_bool($input)) {
+            return $input;
+        }
 
-      return $output;
+        return stripslashes(htmlentities(strip_tags(trim($input))));
     }
-    if (is_bool($input)) {
+
+    /**
+     * _fixIncompleteObject repairs an object if it is incomplete.
+     *
+     * Removes the __PHP_Incomplete_Class crap from the object, so is_object()
+     * will correctly identify $input as an object
+     *
+     * @param object $object The "broken" object
+     * @return object The "fixed" object
+     */
+    private static function _fixIncompleteObject($input)
+    {
+        if (!is_object($input) && gettype($input) == 'object') {
+            return unserialize(serialize($input));
+        }
+
         return $input;
     }
-
-    return stripslashes(htmlentities(strip_tags(trim($input))));
-  }
-
-  /**
-   * _fixIncompleteObject repairs an object if it is incomplete.
-   *
-   * Removes the __PHP_Incomplete_Class crap from the object, so is_object()
-   * will correctly identify $input as an object
-   *
-   * @param object $object The "broken" object
-   * @return object The "fixed" object
-   */
-  private static function _fixIncompleteObject($input)
-  {
-    if (!is_object($input) && gettype($input) == 'object') {
-      return unserialize(serialize($input));
-    }
-
-    return $input;
-  }
-
 }
